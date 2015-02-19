492634437177af9b531073d507318adbbe513d6e
unknown <daniel.filonik@qut.edu.au>
2015-02-19 17:54:48 +1000

    Quickfix.

--- glue/gl/extensions/textures_numpy.py
+++ glue/gl/extensions/textures_numpy.py
@@ -5,6 +5,7 @@ import functools as ft
 import numpy as np
 
 from ...decorators import decorationmethod
+from ...utilities import Unspecified, specified, getspecified
 
 from ..textures import *
 
@@ -13,27 +14,27 @@ from .types_numpy import _nptypes_to_gltypes
 @decorationmethod(Texture2D)
 def set_image(func):
     @ft.wraps(func)
-    def decorated(obj, image, *args, **kwargs):
+    def decorated(obj, image, size=Unspecified, *args, **kwargs):
         if isinstance(image, np.ndarray):
-            size = image.shape
+            size = getspecified(size, image.shape)
             type = _nptypes_to_gltypes[image.dtype.type]
             image = np.flipud(image)
             return func(obj, image, size, type, *args, **kwargs)
         else:
-            return func(obj, image, *args, **kwargs)
+            return func(obj, image, size=size, *args, **kwargs)
     
     return decorated
 
 @decorationmethod(Texture2D)
 def set_sub_image(func):
     @ft.wraps(func)
-    def decorated(obj, image, *args, **kwargs):
+    def decorated(obj, image, size=Unspecified, *args, **kwargs):
         if isinstance(image, np.ndarray):
-            size = image.shape
+            size = getspecified(size, image.shape)
             type = _nptypes_to_gltypes[image.dtype.type]
             image = np.flipud(image)
             return func(obj, image, size, type, *args, **kwargs)
         else:
-            return func(obj, image, *args, **kwargs)
+            return func(obj, image, size=size, *args, **kwargs)
     
     return decorated
--- glue/gl/extensions/textures_pil.py
+++ glue/gl/extensions/textures_pil.py
@@ -6,20 +6,36 @@ import numpy as np
 import PIL.Image
 
 from ...decorators import decorationmethod
+from ...utilities import Unspecified, specified, getspecified
 
 from ..textures import *
 
 @decorationmethod(Texture2D)
 def set_image(func):
     @ft.wraps(func)
-    def decorated(obj, image, *args, **kwargs):
+    def decorated(obj, image, size=Unspecified, *args, **kwargs):
         if PIL.Image.isImageType(image):
             #image = image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
             image = image.convert("RGBA")
-            data = np.array(list(image.getdata()), np.uint8)
+            data = np.array(list(image.getdata()), dtype=np.uint8)
             data = np.reshape(data, image.size + (4,))
-            return func(obj, data, *args, **kwargs)
+            return func(obj, data, size=size, *args, **kwargs)
         else:
-            return func(obj, image, *args, **kwargs)
+            return func(obj, image, size=size, *args, **kwargs)
+    
+    return decorated
+
+@decorationmethod(Texture2D)
+def set_sub_image(func):
+    @ft.wraps(func)
+    def decorated(obj, image, size=Unspecified, *args, **kwargs):
+        if PIL.Image.isImageType(image):
+            #image = image.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+            image = image.convert("RGBA")
+            data = np.array(list(image.getdata()), dtype=np.uint8)
+            data = np.reshape(data, image.size + (4,))
+            return func(obj, data, size=size, *args, **kwargs)
+        else:
+            return func(obj, image, size=size, *args, **kwargs)
     
     return decorated
