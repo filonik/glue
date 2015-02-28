1996679958affe699f3a8253e106b1a88b548fb1
unknown <daniel.filonik@qut.edu.au>
2015-02-28 18:29:02 +1000

    Quickfix.

--- glue/glfw/glfw.py
+++ glue/glfw/glfw.py
@@ -7,6 +7,7 @@ import six
 
 from clibs import glfw3 as GLFW
 
+from ..decorators import attrcached
 from ..flyweights import Resource
 from ..utilities import chdir, Unspecified, specified, getspecified
 
