7cc22f5cf4fc3b9ba49751204e9b53da2830856f
unknown <daniel.filonik@qut.edu.au>
2015-03-02 02:12:48 +1000

    Quickfix.

--- glue/projections.py
+++ glue/projections.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def ortho(l, r, t, b, n, f):
     return np.array([
-        [2/(r-l), 0, 0, -(r+l)/(r-l)],
-        [0, 2/(b-t), 0, -(b+t)/(b-t)],
-        [0, 0, 2/(f-n), -(f+n)/(f-n)],
-        [0, 0, 0, 1]
+        [2/(r-l), 0, 0, 0],
+        [0, 2/(b-t), 0, 0],
+        [0, 0, 2/(f-n), 0],
+        [-(r+l)/(r-l), -(b+t)/(b-t), -(f+n)/(f-n), 1]
     ], dtype=np.float32)
     
 def frustum(l, r, t, b, n, f):
